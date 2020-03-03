3470e82453ee924feb5e1a986506f72fdfce9364
Grant Keller <grant@kellerdigital.com>
2020-03-02 22:16:30 -0800

    Integrated adblock npm module

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-const { app, BrowserWindow } = require('electron')
+"use strict";
+const { app, BrowserWindow, session } = require('electron');
+const { ElectronBlocker } = require('@cliqz/adblocker-electron');
+const fetch = require('node-fetch'); // required 'fetch'
 
-function createWindow () {
+ElectronBlocker.fromPrebuiltAdsAndTracking(fetch).then((blocker) => {
+    blocker.enableBlockingInSession(session.defaultSession);
+});
+
+
+
+async function createWindow () {
     // Create the browser window.
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 450,
+        height: 700,
+        title: 'YouTube Music',
+        icon: './img/ytm_favicon.png',
+        autoHideMenuBar: true,
+        backgroundColor: '#000000',
         webPreferences: {
             nodeIntegration: true
         }
     })
 
-    // and load the index.html of the app.
+    if (session.defaultSession === undefined) {
+        throw new Error('defaultSession is undefined');
+    }
+
+    let blocker = await ElectronBlocker.fromPrebuiltAdsOnly(fetch); // ads only
+    blocker.enableBlockingInSession(session.defaultSession);
+
+    blocker.on('request-blocked', function() {
+        console.log('blocked');
+    });
+
+
+    // load external URL
     win.loadURL('https://music.youtube.com');
 
-    win.addExtension('./extensions/adblock-youtube/5.0.9_0');
-
-
     // Open the DevTools.
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
+
+
+    // Add custom CSS
+    let contents = win.webContents
+    contents.on('did-finish-load', function () {
+        // contents.insertCSS('ytmusic-popup-container {display: none !important;}')
+    })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+app.allowRendererProcessReuse = false
+
+
+// Create app window
 app.on('ready', createWindow)
+
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -41,6 +73,3 @@ app.on('activate', () => {
         createWindow()
     }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
